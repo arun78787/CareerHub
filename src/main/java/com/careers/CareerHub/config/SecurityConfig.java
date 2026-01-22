ca4442d803f91c036fdd24a0cccaebbb9a784740
Arun Yadav <arun9991421407yadav@gmail.com>
2026-01-22 19:49:10 +0530

    implemented jwt authentication filter

--- src/main/java/com/careers/CareerHub/config/SecurityConfig.java
+++ src/main/java/com/careers/CareerHub/config/SecurityConfig.java
@@ -1,25 +1,35 @@
 package com.careers.CareerHub.config;
 
+import com.careers.CareerHub.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth->auth
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
+
         return http.build();
     }
 }
+
--- src/main/java/com/careers/CareerHub/controller/ProjectController.java
+++ src/main/java/com/careers/CareerHub/controller/ProjectController.java
@@ -1,40 +1,38 @@
 package com.careers.CareerHub.controller;
 
 import com.careers.CareerHub.dto.ProjectDto;
+import com.careers.CareerHub.security.CustomUserDetails;
 import com.careers.CareerHub.service.JwtService;
 import com.careers.CareerHub.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/projects")
 @RequiredArgsConstructor
 public class ProjectController {
+
     private final ProjectService projectService;
-    private final JwtService jwtService;
 
     @PostMapping
     public ResponseEntity<?> createProject(
-            @RequestHeader("Authorization") String auth,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody ProjectDto dto
-    ){
-        String token = auth.substring(7);
-        String email = jwtService.extractEmail(token);
-
+    ) {
         return ResponseEntity.ok(
-                projectService.create(dto,email)
+                projectService.create(dto, userDetails.getUsername())
         );
     }
+
     @GetMapping
     public ResponseEntity<?> myProjects(
-            @RequestHeader("Authorization") String auth
+            @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
-        String token = auth.substring(7);
-        String email = jwtService.extractEmail(token);
-
         return ResponseEntity.ok(
-                projectService.getUserProjects(email)
+                projectService.getUserProjects(userDetails.getUsername())
         );
     }
 }
+
